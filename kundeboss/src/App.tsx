d0c38d71c600bb08bf867fc4a5e71e64b3dccd89
krishau99 <kristo9@stud.ntnu.no>
2021-02-05 15:24:32 +0100

    Prøver på login

--- kundeboss/src/App.tsx
+++ kundeboss/src/App.tsx
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://timiansfuncapp.azurewebsites.net"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Log In
         </a>
       </header>
     </div>
