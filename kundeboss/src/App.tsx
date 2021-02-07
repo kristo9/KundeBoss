fd8391fb6203df677a62e3cc105722ef76cf9f14
otvegg <78576061+otvegg@users.noreply.github.com>
2021-02-07 17:56:07 +0100

    Update App.tsx
    Endret på auth link

--- kundeboss/src/App.tsx
+++ kundeboss/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://timiansfuncapp.azurewebsites.net"
+          href="https://timiansfuncapp.azurewebsites.net/.auth/login/aad/callback"
           target="_blank"
           rel="noopener noreferrer"
         >
