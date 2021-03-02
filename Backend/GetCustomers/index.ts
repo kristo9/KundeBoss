888889ba51e1fa0812dbb1cdd7f9c41e7812c7ee
Didrik <didrikkb@stud.ntnu.no>
2021-03-02 12:41:15 +0100

    Endret db tilkobling i backend

--- Backend/GetCustomers/index.ts
+++ Backend/GetCustomers/index.ts
@@ -1,69 +1,70 @@
-import { Context, HttpRequest } from "@azure/functions"
-import { returnResult } from "../SharedFiles/dataValidation";
-import { getKey, options, prepToken, errorQuery, errorUnauthorized } from "../SharedFiles/auth";
-import { verify } from "jsonwebtoken";
-import { DBName, connectRead } from "../SharedFiles/dataBase";
+import { Context, HttpRequest } from '@azure/functions';
+import { returnResult } from '../SharedFiles/dataValidation';
+import { getKey, options, prepToken, errorQuery, errorUnauthorized } from '../SharedFiles/auth';
+import { verify } from 'jsonwebtoken';
+import { connectRead } from '../SharedFiles/dataBase';
+import { Db, Decoded } from '../SharedFiles/interfaces';
 
 module.exports = (context: Context, req: HttpRequest): any => {
-    let employeeId: any;
+  let employeeId: any;
 
-    let token = prepToken(context, req.headers.authorization);
+  let token = prepToken(context, req.headers.authorization);
 
-    if (token === null) {
+  if (token === null) {
+    return context.done();
+  }
+
+  const authorize = (db: Db) => {
+    verify(token, getKey, options, (err: any, decoded: Decoded) => {
+      // verified and decoded token
+      if (err) {
+        errorUnauthorized(context, 'Token not valid');
         return context.done();
-    }
+      } else {
+        employeeId = decoded.preferred_username;
+        functionQuery(db);
+      }
+    });
+  };
 
-    const authorize = (client: { db: (arg0: string) => any }) => {
-        verify(token, getKey, options, (err: any, decoded: { [x: string]: any; }) => {
-            // verified and decoded token
-            if (err) {
-                errorUnauthorized(context, "Token not valid");
-                return context.done();
-            }
-            else {
-                employeeId = decoded.preferred_username;
-                functionQuery(client);
-            }
-        });
-    };
+  const projection = {
+    'name': 1,
+    'employeeId': 1,
+    'customerInformation._id': 1,
+    'customerInformation.name': 1,
+    'customerInformation.contact.name': 1,
+    'customerInformation.contact.mail': 1,
+    'customerInformation.tags': 1
+  };
 
-    const projection = {
-        "name": 1,
-        "employeeId": 1,
-        "customerInformation._id": 1,
-        "customerInformation.name": 1,
-        "customerInformation.contact.name": 1,
-        "customerInformation.contact.mail": 1,
-        "customerInformation.tags": 1
-    };
+  const functionQuery = (db: Db) => {
+    db.collection('employee')
+      .aggregate([
+        {
+          '$match': {
+            'employeeId': employeeId
+          }
+        },
+        {
+          '$lookup': {
+            'from': 'customer',
+            'localField': 'customers.id',
+            'foreignField': '_id',
+            'as': 'customerInformation'
+          }
+        }
+      ])
+      .project(projection)
+      .toArray((error: any, docs: JSON) => {
+        if (error) {
+          errorQuery(context);
+          return context.done();
+        } else {
+          returnResult(context, docs[0]);
+          context.done();
+        }
+      });
+  };
 
-    const functionQuery = (client: { db: (arg0: string) => any }) => {
-        client.db(DBName).collection("employee").aggregate([
-            {
-                "$match": {
-                    "employeeId": employeeId
-                }
-            },
-            {
-                "$lookup":
-                {
-                    "from": "customer",
-                    "localField": "customers.id",
-                    "foreignField": "_id",
-                    "as": "customerInformation"
-                }
-            }
-        ]).project(projection).toArray((error: any, docs: JSON | JSON[]) => {
-            if (error) {
-                errorQuery(context);
-                return context.done();
-            }
-            else {
-                returnResult(context, docs[0]);
-                context.done();
-            }
-        });
-    };
-
-    connectRead(context, authorize);
+  connectRead(context, authorize);
 };
