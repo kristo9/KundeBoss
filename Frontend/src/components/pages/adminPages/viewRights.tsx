3778bc19dfab0a2a1b62c5e58525552a07809dbf
Alexander-AJ <alexanaj@stud.ntnu.no>
2021-03-24 15:18:42 +0100

    startet på adminsiden

--- Frontend/src/components/pages/adminPages/viewRights.tsx
+++ Frontend/src/components/pages/adminPages/viewRights.tsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
+import { getAllEmployees } from '../../../azure/api';
 
-function ViewRights(props: any) {
+function ViewRights() {
   const [employeeData, setEmployeeData] = useState(null);
 
+  //gets the data from the server
+  async function getEmployeeData() {
+    const data = await getAllEmployees();
+    setEmployeeData(data);
+  }
+
+  //getEmployeeData();
   return (
     <div>
       <h1>ViewRights</h1>
-      <p>NOENOENEOEN</p>
-      <DisplayEmployee />
+      {console.log('test')}
+      {console.log(employeeData)}
     </div>
   );
 }
--- Frontend/src/components/pages/supplierPages/supplierPage.tsx
+++ Frontend/src/components/pages/supplierPages/supplierPage.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoadingSymbol from '../../basicComp/loading';
 
 function SupplierPage() {
+  const [pageState, setPageState] = useState(<LoadingSymbol />);
+  const [supplierInfo, setSupplierInfo] = useState(null);
+
+  async function loadSupplierInfo() {
+    // const supplierI =
+  }
+
   return <div>test</div>;
 }
 
